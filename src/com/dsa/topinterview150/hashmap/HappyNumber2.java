c1b2d4ce12bfb34dac655975350145967b2b5439
Wes So <wilsonfso@gmail.com>
2024-05-31 22:40:19 -0600

    Solution to HappyNumber2

--- src/com/dsa/topinterview150/hashmap/HappyNumber2.java
+++ src/com/dsa/topinterview150/hashmap/HappyNumber2.java
@@ -1,12 +1,33 @@
 package com.dsa.topinterview150.hashmap;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber2 {
     public static void main(String[] args) {
+        int n = 2;
+        System.out.println(isHappy(n));
 
     }
 
     public static boolean isHappy(int n) {
-        return false;
+        Set<Integer> set = new HashSet<>();
+        int sum = 0;
+        //continue processing until n != 1 and set doesn't contain n
+        while(n != 1 && !set.contains(n)){
+            System.out.println(n);
+            set.add(n);
+            while(n!=0){
+                sum += Math.pow(n%10,2);
+                n /=10;
+            }
+            n = sum;
+            sum = 0;
+        }
+
+        return n == 1;
+
+
     }
 
 
