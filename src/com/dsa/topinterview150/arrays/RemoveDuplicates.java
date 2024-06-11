2a5b170c82919e181b6f7370ea8e338b378bd728
Wes So <wilsonfso@gmail.com>
2024-06-10 20:37:12 -0600

    Solution to top150 array Problem: RemoveDuplicates

--- src/com/dsa/topinterview150/arrays/RemoveDuplicates.java
+++ src/com/dsa/topinterview150/arrays/RemoveDuplicates.java
@@ -6,8 +6,9 @@ import java.util.Arrays;
 public class RemoveDuplicates {
 
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println(removeDuplicates(nums));
+        //int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(removeDuplicates2(nums));
         System.out.println(Arrays.toString(nums));
     }
 
@@ -19,6 +20,27 @@ public class RemoveDuplicates {
                 val = nums[i];
                 nums[k] = nums[i];
                 k++;
+            }
+        }
+        return k;
+    }
+
+    public static int removeDuplicates2(int[] nums) {
+        int k = 0;
+        int val = Integer.MIN_VALUE;
+        int ctr = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val || ctr < 2) {
+                if(nums[i]!= val){
+                    ctr = 1;
+                } else {
+                    ctr++;
+                }
+                val = nums[i];
+                nums[k] = nums[i];
+                k++;
+            } else {
+                ctr++;
             }
         }
         return k;
